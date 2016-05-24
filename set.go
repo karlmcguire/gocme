05b0bf7ee0c36481b53778779344837283aa163c
Karl McGuire <karl@smo.nu>
2016-05-23 21:06:26 -0400

    added SetBody

---
+++ set.go
@@ -0,0 +1,43 @@
+package gocme
+
+import (
+	"os/exec"
+	"strings"
+)
+
+
+// SetBody replaces the contents of a given window.	
+// TODO: make this cleaner and document what's going on.
+func SetBody(id string, body string) error {
+	winaddr := exec.Command("9p", "write", "acme/"+id+"/addr")
+	winaddr.Stdin = strings.NewReader(",")
+	err := winaddr.Run()
+	if err != nil {
+		return err
+	}
+
+	winctl := exec.Command("9p", "write", "acme/"+id+"/ctl")
+	winctl.Stdin = strings.NewReader("dot=addr")
+	err = winctl.Run()
+	if err != nil {
+		return err
+	}
+
+	winwrite := exec.Command("9p", "write", "acme/"+id+"/wrsel")
+	winwrite.Stdin = strings.NewReader(body)
+	err = winwrite.Run()
+	if err != nil {
+		return err
+	}
+
+	winaddr2 := exec.Command("9p", "write", "acme/"+id+"/addr")
+	winaddr2.Stdin = strings.NewReader("0")
+	err = winaddr2.Run()
+	if err != nil {
+		return err
+	}
+
+	winctl2 := exec.Command("9p", "write", "acme/"+id+"/ctl")
+	winctl2.Stdin = strings.NewReader("dot=addr")
+	return winctl2.Run()
+}
